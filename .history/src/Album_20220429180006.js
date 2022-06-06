b3d57ca1e3b9ff5d6539665e6125843473391c67
lucca <luccavilasboas@hotmail.com>
2022-06-06 13:25:15 -0300

    optimization and study on react admin

---
+++ .history/src/Album_20220429180006.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {
+  List,
+  Datagrid,
+  ShowButton,
+  Show,
+  SimpleShowLayout,
+  ImageField,
+  ReferenceManyField,
+  SingleFieldList,
+  TextField,
+} from 'react-admin';
+
+export const AlbumList = (props) => (
+  <List {...props}>
+    <Datagrid>
+      <TextField source="title" label="Title" />
+    </Datagrid>
+  </List>
+);
+
+export const AlbumShow = (props) => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField disabled source="id" />
+      <TextField source="title" label="Title" />
+      <ReferenceManyField label="Photos" reference="photos" target="albumId">
+        <SingleFieldList>
+          <ImageField source="url" title="thumbnailUrl" src="url" />
+        </SingleFieldList>
+      </ReferenceManyField>
+    </SimpleShowLayout>
+  </Show>
+);
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { Card, CardContent, CardHeader } from '@material-ui/core';
 
 export const Dashboard = (props) => (
-  <Card {...props}>
+  <>
     <CardHeader title="Welcome to the administration" />
-    <CardContent> Welcome admin!!</CardContent>
-  </Card>
+    <CardContent>Welcome admin!!</CardContent>
+  </>
 );
